ad585200b391b5cc8d1b685271b4ae3277a4597b
foolish-pine <matsuda.takuya41@gmail.com>
2023-03-26 23:32:43 +0900

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+// reducer
+
+
+// actions -> increment, decrement
+
+
+// store
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
